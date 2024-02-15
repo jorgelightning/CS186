f8f7bf6634c7eec8b2ad48a579e721fda1d54d88
JP <2220635+jorgelightning@users.noreply.github.com>
2024-02-15 13:39:55 -0800

    Update LabProgram_max.java

--- LabProgram_max.java
+++ LabProgram_max.java
@@ -1,3 +1,22 @@
+// Write a method maxMagnitude() with three integer parameters that returns the largest magnitude value. Use the method in the main program that takes three integer inputs and outputs the largest magnitude value.
+
+// Ex: If the inputs are:
+
+// 5 7 9
+// method maxMagnitude() returns and the main program outputs:
+
+// 9
+// Ex: If the inputs are:
+
+// -17 -8 -2
+// method maxMagnitude() returns and the main program outputs:
+
+// -17
+// Note: The method does not just return the largest value, which for -17 -8 -2 would be -2. Though not necessary, you may use the absolute-value built-in math method.
+
+// Your program must define and call a method:
+// public static int maxMagnitude(int userVal1, int userVal2, int userVal3)
+
 import java.util.Scanner;
 
 public class LabProgram_max {
